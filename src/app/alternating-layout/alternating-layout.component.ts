9adb9a13021004121caf4339c681ce1275896779
murilo-toddy <murilo.todao@usp.br>
2022-01-21 18:34:28 -0300

    Implement html sanitization

--- src/app/alternating-layout/alternating-layout.component.ts
+++ src/app/alternating-layout/alternating-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core'
+// import { ViewEncapsulation } from '@angular/compiler'
+import { Component, Input, ViewEncapsulation } from '@angular/core'
 
 interface alternatingLayout {
   image?: {
