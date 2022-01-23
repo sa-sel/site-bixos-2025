b307eb6807cfae1caa272346972c3f9f212850ab
Lucas Viana <oviana@outlook.com>
2022-01-22 21:38:28 -0300

    #19 - Formatting

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -3,4 +3,3 @@ export * from './logo/logo.component'
 export * from './modal/modal.component'
 export * from './social-media-icon/social-media-icon.component'
 export * from './title/title.component'
-
