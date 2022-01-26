d4bc370da19ff752a26004a89612a79643aa5393
murilo-toddy <murilo.todao@usp.br>
2022-01-26 13:55:15 -0300

    Implement responsive design

--- src/app/models/alternating-layout.model.ts
+++ src/app/models/alternating-layout.model.ts
@@ -1,14 +1,7 @@
 import { ImageModel } from './image.model'
 
-export interface Style {
-  customImageClasses?: string
-  customTextClasses?: string
-}
-
 export interface AlternatingLayoutModel {
   image?: ImageModel
   text?: string
-  imageStyle?: string
-  textStyle?: string
   imageOnLeft?: boolean
 }
--- src/app/shared/alternating-layout/alternating-layout.component.ts
+++ src/app/shared/alternating-layout/alternating-layout.component.ts
@@ -19,37 +19,4 @@ export class AlternatingLayoutComponent implements OnInit {
       return item
     })
   }
-
-  getCustomTextClasses = (
-    item: AlternatingLayoutModel,
-    imageOnLeft: boolean,
-    textHeight: number,
-    imageHeight: number
-  ): string => {
-    if (item.textStyle === undefined) {
-      if (textHeight < imageHeight) {
-        const order = imageOnLeft ? 'order-2' : 'order-1'
-        item.textStyle = `px-5 w-50 ${order}`
-      } else {
-        item.textStyle = 'w-100'
-      }
-    }
-    return item.textStyle
-  }
-
-  getCustomImageClasses = (
-    item: AlternatingLayoutModel,
-    imageOnLeft: boolean,
-    textHeight: number,
-    imageHeight: number
-  ): string => {
-    if (item.imageStyle === undefined) {
-      if (textHeight < imageHeight) {
-        item.imageStyle = imageOnLeft ? 'order-1' : 'order-2'
-      } else {
-        item.imageStyle = imageOnLeft ? 'float left' : 'float right'
-      }
-    }
-    return item.imageStyle
-  }
 }
